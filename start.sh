a109593d6cc6ff5b390e35c92f83e14f60d64d04
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-27 19:41:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/the_ocr_bot /LazyDeveloper
+git clone https://github.com/konichiwa55115/ocr511551151 /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
