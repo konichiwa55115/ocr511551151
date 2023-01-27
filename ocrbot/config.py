b690e15fe784ade0c1178d1f0d17ccba7575259a
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-27 19:41:36 +0200

    Update config.py

--- ocrbot/config.py
+++ ocrbot/config.py
@@ -4,4 +4,4 @@ import dotenv
 dotenv.load_dotenv()
 
 API_KEY = "K85900838188957" # api key from https://ocr.space/ocrapi
-BOT_TOKEN = "5521769462:AAFyfa9i2gaGYri_ty5ITxJTLRHYhfl4reQ" # bot token from @BotFather
+BOT_TOKEN = "5442625381:AAEi2yJ9MXB_FcgpWWoxe7mBaTJTZRGDH4k" # bot token from @BotFather
